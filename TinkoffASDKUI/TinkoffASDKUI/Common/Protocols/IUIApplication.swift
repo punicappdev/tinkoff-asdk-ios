47daab4b57ea00694a03677591a38f3774047e1f
punicappdev <88042995+punicappdev@users.noreply.github.com>
2025-03-26 12:19:55 +0200

    Update IUIApplication.swift

--- TinkoffASDKUI/TinkoffASDKUI/Common/Protocols/IUIApplication.swift
+++ TinkoffASDKUI/TinkoffASDKUI/Common/Protocols/IUIApplication.swift
@@ -19,4 +19,9 @@ extension IUIApplication {
     }
 }
 
-extension UIApplication: IUIApplication {}
+extension UIApplication: IUIApplication {
+    func open(_ url: URL, options: [OpenExternalURLOptionsKey : Any], completionHandler
+        completion: ((Bool) -> Void)?) {
+        open(url, options: [:], completionHandler: completion)
+    }
+}
